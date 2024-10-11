ce6873e2fdbb3d15e1cc48e87c3de20ae9eb9849
FishHawk <whang23@outlook.com>
2024-10-11 20:16:36 +0800

    update

--- combine.py
+++ combine.py
@@ -3,30 +3,45 @@ def read_file(filename):
         return f.read().splitlines()
 
 
-def cb(zh, zh9, target):
-    print(f"{zh} + {zh9} => {target}")
-    lines_zh = read_file(zh)
-    lines_zh9 = [l for l in read_file(zh9) if l]
+def diff(diff_file, tb_files):
+    tb_text_list = [read_file(f) for f in tb_files]
+    text_output = list(tb_files)
+    text_output.append("")
 
-    lines_target = []
-    count = 0
-    for lines in lines_zh:
-        if lines:
-            lines_target.append(lines)
-            count += 1
-            if count % 3 == 0:
-                lines_target.append(lines_zh9.pop(0))
-        else:
-            lines_target.append("")
+    for i in range(len(tb_text_list[0])):
+        if not tb_text_list[0][i]:
+            continue
+        for t in tb_text_list:
+            text_output.append(t[i])
+        text_output.append("")
 
-    with open(target, "w") as f:
-        f.write("jp\nsakura-0.8\nsakura-0.9\nsakura-0.9.1\n\n")
-        f.write("\n".join(lines_target))
+    with open(diff_file, "w") as f:
+        f.write("\n".join(text_output))
+
+
+def report(report_file, tb_files):
+    tb_text_list = [read_file(f) for f in tb_files]
+    text_output = [f"# {tb_files[0]}\n"] + [f"- {f}" for f in tb_files] + [""]
+
+    count = 1
+    for i in range(len(tb_text_list[0])):
+        if not tb_text_list[0][i]:
+            continue
+        text_output.append(f"### P{count}\n")
+        for t in tb_text_list:
+            text_output.append(" " * 4 + t[i] + "\n")
+        text_output.append("")
+        count += 1
+
+    with open(report_file, "w") as f:
+        f.write("\n".join(text_output))
 
 
 for i in range(1, 5):
-    cb(
-        f"tb{i}.translation.txt",
-        f"zh.Ys.tb{i}.txt",
-        f"tb{i}.translation.txt",
-    )
+    tb_files = [
+        f"data/tb{i}.txt",
+        f"data/zh.Ys.tb{i}.v0.9.2.txt",
+        f"data/zh.Ys.tb{i}.v1.0.txt",
+    ]
+    diff(f"tb{i}.diff.v1.0.md", tb_files)
+    report(f"tb{i}.report.v1.0.md", tb_files)
